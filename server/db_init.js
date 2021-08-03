da4609ea85cef57528594ecab5785d59190f32f7
Oleg Medushevskiy <oleg.medushevskiy@wunderkind.co>
2021-08-03 16:27:16 +0300

    Fix sequelize settings

--- server/db_init.js
+++ server/db_init.js
@@ -2,11 +2,14 @@ const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
   {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+    }
   }
 )
